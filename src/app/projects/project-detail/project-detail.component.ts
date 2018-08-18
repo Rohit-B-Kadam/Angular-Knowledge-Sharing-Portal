4c6e5a497111fac69ffe8a1944dc22d82f0c37f2
Rohit kadam <rohitkadam1407@gmail.com>
2018-08-19 00:10:37 +0530

    change on existing project

--- src/app/projects/project-detail/project-detail.component.ts
+++ src/app/projects/project-detail/project-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { ActivatedRoute, Params, Router, NavigationEnd } from '@angular/router';
 
 import { Project } from '../project.model';
 import { ProjectService } from '../project.service';
@@ -12,13 +12,29 @@ export class ProjectDetailComponent implements OnInit
 {
   pro: Project;
   id: number;
+  private innerWidth: any;
+
+    // Get Device size (If you wanna keep it updated on resize)
+  @HostListener('window:resize', ['$event']) onResize(event) { this.innerWidth = window.innerWidth; }
+
 
   constructor(private projectService: ProjectService,
               private route: ActivatedRoute,
               private router: Router) {
+        // Focus on this component (use only on device screen less than 720)
+        router.events.subscribe(s => {
+          if (s instanceof NavigationEnd) {
+            const tree = router.parseUrl(router.url);
+            if (tree.fragment && this.innerWidth <= 720) {
+              const element = document.querySelector("#" + tree.fragment);
+              if (element) { element.scrollIntoView(true); }
+            }
+          }
+        });
   }
 
   ngOnInit() {
+    // Take detail from url
     this.route.params
       .subscribe(
         (params: Params) => {
@@ -26,6 +42,9 @@ export class ProjectDetailComponent implements OnInit
           this.pro = this.projectService.getProject(this.id);
         }
       );
+    
+      // To Get innerwidth size of device
+      this.innerWidth = window.innerWidth;
   }
 
   // Listener to add prerequsite of project
